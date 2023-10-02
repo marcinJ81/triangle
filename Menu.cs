94f7048f8ab11575bf5c8f465b8bc7c47eeaab92
Marcin Juranek <34885638+marcinJ81@users.noreply.github.com>
2023-10-02 12:31:26 +0200

    first commit

---
+++ Menu.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle3
+{
+	internal class Menu
+	{
+		//		int exit = 0;
+		//		int options = -1;
+		//			while(exit != 9)
+		//			{
+		//				Console.WriteLine("Menu");
+		//				Console.WriteLine("Set 1 - heih trangle");
+		//				Console.WriteLine("Set 2 - heih trangle2");
+		//				Console.WriteLine("Set 3 - side from square ");
+		//				Console.WriteLine("Set 4 - side from square2 ");
+		//				Console.WriteLine("Set 5 - compare the area of the triangles");
+		//				Console.WriteLine("Set 6 - compare the area of the squers");
+		//				Console.WriteLine("Set 9 - exit");
+		//				Console.WriteLine("\n");
+		//				options = Convert.ToInt32(Console.ReadLine());
+
+		//				switch (options)
+		//				{
+		//					case 1:
+		//						try 
+		//						{
+		//							Console.WriteLine("Set heigh trangle");
+		//							h = Convert.ToDouble(Console.ReadLine());
+		//							Console.WriteLine("Set base trangle");
+		//							a = Convert.ToDouble(Console.ReadLine());
+		//							triangle1 = new Triangle(a, h);
+		//							break;
+		//						}
+		//						catch (Exception ex)
+		//						{
+		//							Console.WriteLine(ex.Message);
+		//							break;
+		//						}
+		//					case 2:
+		//						try
+		//{
+		//	Console.WriteLine("Set heigh trangle2");
+		//	h = Convert.ToDouble(Console.ReadLine());
+		//	Console.WriteLine("Set base trangle");
+		//	a = Convert.ToDouble(Console.ReadLine());
+		//	triangle2 = new Triangle(a, h);
+		//	break;
+		//}
+		//catch (Exception ex)
+		//{
+		//	Console.WriteLine(ex.Message);
+		//	break;
+		//}					
+		//					case 3:
+		//						try
+		//{
+		//	Console.WriteLine("Set side from square ");
+		//	sideSquare = Convert.ToDouble(Console.ReadLine());
+		//	square1 = new Square(sideSquare);
+		//	break;
+		//}
+		//catch (Exception ex)
+		//{
+		//	Console.WriteLine(ex.Message);
+		//	break;
+		//}
+
+		//					case 4:
+		//						try
+		//{
+		//	Console.WriteLine("Set side from square2 ");
+		//	sideSquare = Convert.ToDouble(Console.ReadLine());
+		//	square2 = new Square(sideSquare);
+		//	break;
+		//}
+		//catch (Exception ex)
+		//{
+		//	Console.WriteLine(ex.Message);
+		//	break;
+		//}
+
+		//					case 5:
+		//						Console.WriteLine("compare the area of the triangles");
+		//if ((triangle1.A == -1 && triangle1.H == -1) || (triangle2.A == -1 && triangle2.H == -1))
+		//{
+		//	Console.WriteLine("Set value for heigh and base for triangle");
+		//	break;
+		//}
+		//if (triangle1.Area() > triangle2.Area())
+		//{
+		//	Console.WriteLine("triangle1 area is biger then triangle2 area");
+		//}
+		//else if (triangle1.Area() < triangle2.Area())
+		//{
+		//	Console.WriteLine("triangle2 area is biger then triangle1");
+		//}
+		//else
+		//{
+		//	Console.WriteLine("Both are equal");
+		//}
+		//Console.WriteLine("\n");
+		//break;
+
+		//					case 6:
+		//						Console.WriteLine("compare the area of the square");
+		//if (square1.A == -1 && square2.A == -1)
+		//{
+		//	Console.WriteLine("Set value for heigh and base for square");
+		//	break;
+		//}
+		//if (square1.Area() > square2.Area())
+		//{
+		//	Console.WriteLine("square1 area is biger then square2 area");
+		//}
+		//else if (square1.Area() < square2.Area())
+		//{
+		//	Console.WriteLine("square2 area is biger then square1");
+		//}
+		//else
+		//{
+		//	Console.WriteLine("Both are equal");
+		//}
+		//Console.WriteLine("\n");
+		//break;
+
+		//					case 9:
+		//						Console.WriteLine("Koniec");
+		//exit = 9;
+		//break;
+		//				}
+		//			}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Triangle3
+{
+	internal class Program
+	{
+		public delegate double Area();
+		static void Main(string[] args)
+		{
+			Triangle triangle1 = new Triangle();
+			Triangle triangle2 = new Triangle();
+			Square square1 = new Square(), square2 = new Square();
+			int chooise = -1;
+            double a, h, sideSquare;
+            double areaTriangle1, areaSquare1, areaTriangle2, areaSquare2 = 0;
+            while (chooise < 9)
+			{ 
+				Console.WriteLine("Wybierz rodzaj figury");
+				Console.WriteLine("1 - trojakt");
+				Console.WriteLine("2 - kwadrat");
+                Console.WriteLine("3 - licz pola i porównaj");
+                chooise = Convert.ToInt32(Console.ReadLine());
+
+				switch (chooise)
+				{
+					case 1:
+
+						Console.WriteLine("Podaj wymiary pierwszego trójkąta");
+						Console.WriteLine("Podstawę");
+						a = Convert.ToDouble(Console.ReadLine());
+						Console.WriteLine("Wysokość");
+						h = Convert.ToDouble(Console.ReadLine());
+						triangle1 = new Triangle(a, h);
+
+
+                        Console.WriteLine("Podaj wymiary pierwszego trójkąta");
+                        Console.WriteLine("Podstawę");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Wysokość");
+                        h = Convert.ToDouble(Console.ReadLine());
+                        triangle2 = new Triangle(a, h);
+
+                        break;
+
+					case 2:
+
+						Console.WriteLine("Podaj wymiary prostokąta");
+						Console.WriteLine("Bok");
+						a = Convert.ToDouble(Console.ReadLine());
+						square1 = new Square(a);
+
+                        Console.WriteLine("Podaj wymiary prostokąta");
+                        Console.WriteLine("Bok");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        square2 = new Square(a);
+                        break;
+
+					case 3:
+						
+
+						Console.WriteLine("Licz pole figur");
+						//Area d = triangle1.Area;
+                       // areaTriangle1 = d();
+						Console.WriteLine($"pole trójkąta wynosi {triangle1.Area()}");
+
+						//areaSquare1 = (d = square1.Area)();
+						Console.WriteLine($"pole kwadrata wynosi {square1.Area()}");
+                        
+                      
+                        Console.WriteLine($"pole kwadrata wynosi {square2.Area()}");
+
+                        
+                        Console.WriteLine($"pole trójkąta wynosi {triangle2.Area()}");
+                        break;
+				}
+            }
+        }
+	}
+
+	public class CompareObject
+	{
+        public  bool EqualsType(object obj, object obj2)
+        {
+            if (obj == null && obj2 == null)
+			{
+				return false;
+			}
+
+			if ( obj.GetType() != obj2.GetType())
+			{
+				return false;
+			}
+
+			return true;
+        }
+    }
+
+
+	//to value object ??
+	public class Square
+	{ 
+		//A change to Side
+		public double A { get; private set; }
+		public Square()
+		{
+			A = -1;
+		}
+		public Square(double a)
+		{
+			
+			A = a;
+		}
+
+		public double Area()
+		{
+			return A * A;
+		}
+	}
+
+    //to value object ??
+    public class Triangle
+	{
+		//A change to base
+		public double A { get; private set; }
+		//H change to height
+		public double H { get; private set; }
+		public Triangle()
+		{
+			A = -1;
+			H = -1;
+		}
+		public Triangle(double a, double h)
+		{
+			
+			A = a;
+			H = h;
+		}
+
+		public double Area()
+		{
+			return (A * H) / 2;
+		}
+	}
+
+	public class BuildFigures
+	{
+		public List<Triangle> triangles { get; private set; }
+		public List<Square> squares  { get; private set; }
+
+		private Validation validation { get; set; }
+        public BuildFigures(Validation validation)
+        {
+			triangles = new List<Triangle> ();	
+			squares = new List<Square> ();
+			this.validation = validation;
+        }
+
+		public void CreateNewTriangle(double a, double h)
+		{
+            if (a <= 0)
+            {
+                throw new ArgumentException("the length of the base of the triangle must be greater than zero");
+            }
+            if (h <= 0)
+            {
+                throw new ArgumentException("height must be greater than zero");
+            }
+            triangles.Add(new Triangle(a,h)); 
+		}
+
+		public void CreateNewSquare(double a)
+		{
+			validation.ValidationLength(a, "square", );
+            if (a <= 0)
+            {
+                throw new ArgumentException("the length of the side of the square must be greater than zero");
+            }
+			squares.Add( new Square(a));
+        }
+    }
+
+	public class Validation
+	{
+		//value get from reflection
+		public void ValidationLength(double a, string typeObject, string typeValue)
+		{
+			if (a <= 0)
+			{
+				throw new ArgumentException($"the length of the {typeValue} of the {typeObject} must be greater than zero");
+			}
+		}
+	}
+}
